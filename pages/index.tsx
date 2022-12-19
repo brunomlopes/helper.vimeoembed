4d4bb68d7549b64750b9339365ff683a62f2a365
Bruno Lopes <brunomlopes@gmail.com>
2022-12-19 12:22:21 +0000

    v1

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,10 +2,44 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '../styles/Home.module.css'
+import { ChangeEvent, ChangeEventHandler, useEffect, useState } from 'react'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  const [input, setInput] = useState("");
+  const [output, setOutput] = useState("");
+
+  useEffect( ()=>{
+    const fetchData = async ()=>{
+      var result = await Promise.all(input.split("\n")
+      .map(async s => {
+        let url = s.trim();
+        if(!url.startsWith("http")){
+          console.warn(`Line '${url}' is not an url`);
+          return "";
+        }
+        let result :{thumbnail_url:string, video_id:string} = (await (await fetch(`http://vimeo.com/api/oembed.json?url=${url}`)).json());
+        let embedHtml=`<img class="ic-youtube" data-video="vimeo" src="${result['thumbnail_url']}" data-vimeo-id="${result['video_id']}">`
+        return embedHtml;
+      }));
+      let val = "";
+      
+      
+      setOutput(result.join("\n"));
+    }
+
+    fetchData().catch(console.error);
+    let val = input;
+    val =`Hey, val was ${val}`
+    setOutput(val);
+  }, [input])
+
+    const processInput = (input:ChangeEvent<HTMLTextAreaElement>)=>{
+      let val = input.target.value;
+      setInput(val);
+    }
+
   return (
     <>
       <Head>
@@ -16,106 +50,36 @@ export default function Home() {
       </Head>
       <main className={styles.main}>
         <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>pages/index.tsx</code>
-          </p>
           <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
+            <textarea
+              name="keymap_input"
+              id="keymap_input"
+              cols={30}
+              rows={10}
+              className={styles.input_textarea}
+              value={input}
+              onChange={processInput}
+            ></textarea>
           </div>
+          
         </div>
 
         <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-          <div className={styles.thirteen}>
-            <Image
-              src="/thirteen.svg"
-              alt="13"
-              width={40}
-              height={31}
-              priority
-            />
-          </div>
+        <div>
+            <textarea
+              name="keymap_input"
+              id="keymap_input"
+              readOnly
+              cols={30}
+              rows={10}
+              className={styles.input_textarea}
+              value={output}
+            ></textarea>
+          </div>        
         </div>
 
         <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
 
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
         </div>
       </main>
     </>
